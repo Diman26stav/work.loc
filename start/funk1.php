dee2cee360667360b4b2e664b2e3811d67602f89
GazetteHan <sokolo2024@mail.ru>
2023-03-06 16:51:47 +0300

    ФУНКЦИИ

--- start/funk1.php
+++ start/funk1.php
@@ -7,5 +7,32 @@ function ploshad($a,$b){
 }
 
 ploshad(3,7);
-ploshad(45,7);
-ploshad(5,7);
+
+
+
+// 1. Периметр прямоугольника
+// 2. Периметр треугольника
+// 3. Диаметр круга, имея радиус
+
+
+function pram($a,$b,$d){
+    $s = $a * $b * $d;
+    echo "Периметр прямоугольника равен " . $s . "<br>";
+}
+Pram(5,7,9);
+
+
+function treug($a,$b,$d){
+    $s = $a * $b * $d;
+    echo "Периметр треугольника равен " . $s . "<br>";
+}
+
+treug(4,6,8);
+
+function diam($radius)
+{
+    $diametr = $radius * 2;
+    echo "Диаметр круга = " . $diametr . "<br>";
+}
+    diam(5);
+
