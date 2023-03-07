0df0f1247b0d98b2a2e80d7058783136da6d7f1b
GazetteHan <sokolo2024@mail.ru>
2023-03-07 16:31:18 +0300

    Преобразование рубли и доллары

--- start/funk1.php
+++ start/funk1.php
@@ -6,7 +6,7 @@ function ploshad($a,$b){
     echo "Площадь равна " . $s . "<br>";
 }
 
-ploshad(3,7);
+ploshad(13,17);
 
 
 
@@ -19,20 +19,41 @@ function pram($a,$b,$d){
     $s = $a * $b * $d;
     echo "Периметр прямоугольника равен " . $s . "<br>";
 }
-Pram(5,7,9);
+Pram(3,7,9);
 
 
 function treug($a,$b,$d){
-    $s = $a * $b * $d;
+    $s = $a + $b + $d;
     echo "Периметр треугольника равен " . $s . "<br>";
 }
 
-treug(4,6,8);
+treug(77,57,99);
 
 function diam($radius)
 {
-    $diametr = $radius * 2;
+    $diametr = $radius * 5;
     echo "Диаметр круга = " . $diametr . "<br>";
 }
-    diam(5);
+    diam(99);
+
+
+function rub($a,$b){
+    $s = $b * $a;
+
+    echo $b." Рублей равен " . $s . " Долларов  <br>";
+}
+rub(75,5);
+
+
+function dollar($a,$b){
+    $s = $b / $a;
+    $f = round($s,2);
+    echo $b." Долларов равен " . $f . " Рублей <br>";
+}
+dollar(75,5);
+
+
+
+
+
 
